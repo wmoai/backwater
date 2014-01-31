bf870fdb1982fa41f9e9cd434af60f63cb97af6c
t_watase <t_watase@okwave.co.jp>
2014-01-31 19:15:47 +0900

    add attack

--- app.js
+++ app.js
@@ -13,6 +13,9 @@ app.get('/', function (req, res) {
   res.sendfile(__dirname + '/index.html');
 });
 
+
+var charas = new Array();
+
 var Chara = function(socket) {
   this.socket = socket;
   this.point = {
@@ -20,53 +23,98 @@ var Chara = function(socket) {
     y : 10
   };
   this.moving = false;
-  this.v = 50;
+  this.v = 100;
+  this.range = 100;
+  this.hp = 200;
+  this.maxhp = 200;
+  this.str = 10;
 
   this.put = function() {
     console.log(socket.id);
-    io.sockets.emit('set you', {point: this.point, id:socket.id});
+    io.sockets.emit('set chara', {
+      id:socket.id
+      , point: this.point
+      , maxhp: this.maxhp
+      , hp: this.hp
+      , range: this.range
+    });
   }
 
   this.move = function() {
     if (this.moving) {
       var dx = this.next.x - this.point.x;
       var dy = this.next.y - this.point.y;
-      var h = Math.sqrt(Math.pow(dx,2) + Math.pow(dy,2));
+      var theta = Math.atan2(dy, dx);
+      var h = dy;
+      if (dx != 0) {
+        h = dx / Math.cos(theta);
+      }
       if (this.v >= h) {
         this.point = this.next;
         this.moving = false;
       } else {
-        var r = this.v / h;
-        var nx = Math.round(dx * r + this.point.x);
-        var ny = Math.round(dy * r + this.point.y);
-        this.point.x = nx;
-        this.point.y = ny;
+        this.point.x += Math.round(Math.cos(theta) * this.v);
+        this.point.y += Math.round(Math.sin(theta) * this.v);
       }
-      io.sockets.emit('move you', {point: this.point, id:this.socket.id});
+      io.sockets.emit('anyone move', {
+        id: this.socket.id
+      , point: this.point
+      });
+
     }
+  };
+  this.attack = function() {
+    var self = this;
+    charas.forEach(function(c) {
+      if (self != c) {
+        var dx = self.point.x - c.point.x;
+        var dy = self.point.y - c.point.y;
+        var h = dy;
+        if (dx != 0) {
+          var theta = Math.atan2(dy, dx);
+          h = dx / Math.cos(theta);
+        }
+        if (self.range >= h) {
+          console.log("atack from %s to %s", self.socket.id, c.socket.id);
+          c.hp -= self.str;
+          io.sockets.emit('anyone attack', {
+            id: c.socket.id
+            , maxhp: c.maxhp
+            , hp: c.hp
+          });
+        }
+      }
+    });
   };
   var timer;
   this.run = function() {
     var self = this;
     timer = setInterval(function(){
       self.move();
+      self.attack();
+      if (self.hp <= 0) {
+        self.die();
+      }
     }, 1000);
   };
   this.die = function() {
     clearInterval(timer);
     this.socket.emit('die', {id:this.socket.id});
     this.socket.broadcast.emit('die', {id:this.socket.id});
+    for (var i=0; i<charas.length; i++) {
+      if (this == charas[i]) {
+        charas.splice(i, 1);
+      }
+    }
   }
 }
 
-var charas = new Array();
-
 io.sockets.on('connection', function (socket) {
   var chara = new Chara(socket);
-  charas[socket.id] = chara;
-  for (var c in charas) {
-    charas[c].put();
-  };
+  charas.push(chara);
+  charas.forEach(function(c) {
+    c.put();
+  });
   socket.on('field tap', function (data) {
     chara.next = data;
     chara.moving = true;
@@ -75,8 +123,11 @@ io.sockets.on('connection', function (socket) {
 
   socket.on('disconnect', function() {
     console.log('disconnect');
-    charas[socket.id].die();
-    delete charas[socket.id];
+    for (var i=0; i<charas.length; i++) {
+      if (chara == charas[i]) {
+        charas[i].die();
+      }
+    }
   });
 });
 
--- public/client.js
+++ public/client.js
@@ -1,25 +1,49 @@
 $(function() {
-  var socket = io.connect('http://localhost');
-  socket.on('set you', function (data) {
+  var socket = io.connect('http://192.168.24.85:3000');
+
+  var sethp = function(data, chara) {
+    if (!chara) {
+      chara = $('.chara[data-sid='+data.id+']');
+    }
+    var baseWidth = chara.find('.hp').width();
+    chara.find('.hp > .remain').css({
+      width: data.hp / data.maxhp * baseWidth
+    });
+  };
+
+  socket.on('set chara', function (data) {
     if (!$('.chara[data-sid='+data.id+']').size()) {
-      var chara = $('<div>').addClass('chara').attr('data-sid',data.id).offset({
+      var chara = $('#chara').clone().removeAttr('id').attr('data-sid',data.id).offset({
         top: data.point.y,
         left: data.point.x
       });
       $('#field').append(chara);
+      chara.find('.range').css({
+        width: data.range*2
+        , height: data.range*2
+        , top: -data.range
+        , left: -data.range
+        , 'border-radius': data.range
+      });
+      sethp(data, chara);
     }
   });
 
   $('#field').on('click', function(e) {
     var fp = $(this).position();
-    socket.emit('field tap', { x: e.clientX - fp.left, y: e.clientY - fp.top });
+    socket.emit('field tap', { x: e.pageX - fp.left, y: e.pageY - fp.top });
   });
 
-  socket.on('move you', function (data) {
-    $('.chara[data-sid='+data.id+']').animate({
+  socket.on('anyone move', function (data) {
+    var my = $('.chara[data-sid='+data.id+']');
+    my.animate({
       top: data.point.y,
       left: data.point.x
     }, 1000, "linear");
+  });
+
+  socket.on('anyone attack', function (data) {
+    sethp(data);
   });
 
   socket.on('die', function (data) {
